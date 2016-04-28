7191ee764ea24bf3704fcd3f53070a47b2ce6064
wjcgithub <wjc163@sina.cn>
2016-04-28 11:16:49 +0800

    添加单例模式代码

---
+++ Singleton/Singleton.php
@@ -0,0 +1,34 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * Author: evolution
+     * Date: 16-4-27
+     * Time: 下午5:50.
+     *
+     * license GPL
+     */
+    class Singleton
+    {
+        private static $instance;
+        public $name = 'lisi';
+
+        private function __construct()
+        {
+            echo 'can not instance me!';
+        }
+
+        private function __clone()
+        {
+            echo 'can not clone me!';
+        }
+
+        public static function getInstance()
+        {
+            if (empty(self::$instance)) {
+                self::$instance = new Singleton();
+            }
+
+            return self::$instance;
+        }
+    }
